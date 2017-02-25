4b3c286ff96ad0c4374592c4d4e60f676d84bc7c
amadhuri <athota.madhuri@gmail.com>
2017-02-24 21:43:48 -0800

    Show toast when user does not enter user name. Clean up unnecessary code.

--- app/src/main/java/com/capstone/multiplicationwizard/layout/NewUserFragment.java
+++ app/src/main/java/com/capstone/multiplicationwizard/layout/NewUserFragment.java
@@ -12,7 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.capstone.multiplicationwizard.GameActivity;
 import com.capstone.multiplicationwizard.MainActivity;
@@ -33,16 +33,11 @@ public class NewUserFragment extends Fragment {
 
     View mView = null;
 
-    private OnFragmentInteractionListener mListener;
-
-    public NewUserFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         mView =  inflater.inflate(R.layout.fragment_new_user, container, false);
         Button mContBtn = (Button)mView.findViewById(R.id.bt_new_user_cont);
@@ -50,58 +45,25 @@ public class NewUserFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 EditText tvNewUser = (EditText)mView.findViewById(R.id.new_user_et);
-                Log.e("NewUserFragment","New USer name:"+tvNewUser.getText());
-                MainActivity mainActivity = (MainActivity)getActivity();
-                //long id = mainActivity.mwDB.createUser(newUser);
-                ContentValues contentValues = new ContentValues();
-                contentValues.put(MWSQLiteHelper.KEY_USERNAME, tvNewUser.getText().toString());
-                Uri id = mainActivity.getContentResolver().insert(MWItemsContract.USERS_CONTENT_URI,contentValues);
-                Log.e("NewUserFragment","createUSer returned id:"+id);
-                //Launch GameActivity
-                Intent intent = new Intent(mainActivity.getApplicationContext(), GameActivity.class);
-                startActivity(intent);
-                return;
+                Log.e("NewUserFragment","New User name:"+tvNewUser.getText());
+                if(tvNewUser.getText().toString().equals("")) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Enter user name", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    MainActivity mainActivity = (MainActivity) getActivity();
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(MWSQLiteHelper.KEY_USERNAME, tvNewUser.getText().toString());
+                    Uri id = mainActivity.getContentResolver().insert(MWItemsContract.USERS_CONTENT_URI, contentValues);
+                    Log.e("NewUserFragment", "create user returned id:" + id);
+                    //Launch GameActivity
+                    Intent intent = new Intent(mainActivity.getApplicationContext(), GameActivity.class);
+                    startActivity(intent);
+                }
+
             }
         });
         return mView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-           /* throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");*/
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
