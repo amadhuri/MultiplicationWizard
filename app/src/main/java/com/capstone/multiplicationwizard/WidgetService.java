b87bcae48191b738864c795306d8bddd2c7321a6
amadhuri <athota.madhuri@gmail.com>
2017-05-21 17:58:10 -0700

    modified app widget service

--- app/src/main/java/com/capstone/multiplicationwizard/WidgetService.java
+++ app/src/main/java/com/capstone/multiplicationwizard/WidgetService.java
@@ -5,14 +5,17 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+import android.widget.TextView;
+
+import com.capstone.multiplicationwizard.data.MWSQLiteHelperNew;
+import com.capstone.multiplicationwizard.model.User;
+
+import java.util.ArrayList;
 
 public class WidgetService extends RemoteViewsService {
-    public WidgetService() {
-    }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return super.onBind(intent);
+    public WidgetService() {
+
     }
 
     @Override
@@ -21,13 +24,16 @@ public class WidgetService extends RemoteViewsService {
     }
     class WidgetViewFactory implements RemoteViewsFactory {
 
-        public WidgetViewFactory() {
+        MWSQLiteHelperNew mwDB;
+        ArrayList<User> userArrayList;
+        int numOfUsers;
 
+        public WidgetViewFactory() {
         }
 
         @Override
         public void onDestroy() {
-
+            userArrayList.clear();
         }
 
         @Override
@@ -47,21 +53,31 @@ public class WidgetService extends RemoteViewsService {
 
         @Override
         public void onCreate() {
-
+            mwDB = new MWSQLiteHelperNew(getApplicationContext());
+            userArrayList = mwDB.getUsers();
+            numOfUsers = userArrayList.size();
         }
 
         @Override
         public int getViewTypeCount() {
-            return 0;
+            return 1;
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return numOfUsers;
         }
 
         @Override
         public RemoteViews getViewAt(int i) {
+            if(i < numOfUsers) {
+                User user = userArrayList.get(i);
+                RemoteViews remoteViews = new RemoteViews(getPackageName(),R.layout.appwidget_layout);
+                remoteViews.setTextViewText(R.id.tv_appwidget_1,user.getUsername());
+                remoteViews.setTextViewText(R.id.tv_appwidget_2,user.getHighScore().toString());
+
+                return remoteViews;
+            }
             return null;
         }
 
